a3b763b4c393d2b8ff97b074b9ca0bbb3655c3ce
fezimpaza <145721008+Fezekile01@users.noreply.github.com>
2026-03-07 21:29:30 +0200

    models.py

--- models.py
+++ models.py
@@ -8,10 +8,18 @@ class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-    department =db.Column(db.string(50))
-    level_of_study=db.Column(db.string)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    course = db.Column(db.String(100))
+    department =db.Column(db.string(50))
+    level_of_study=db.Column(db.string)
+
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
 
 
 class Scholarship(db.Model):
@@ -21,6 +29,7 @@ class Scholarship(db.Model):
     title = db.Column(db.String(200))
     description = db.Column(db.Text)
     deadline = db.Column(db.String(50))
+    closing_date=db.Colomn(date))
 
 
 class Application(db.Model):
